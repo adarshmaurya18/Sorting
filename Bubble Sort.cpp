18f762fdd0be9812b73e107152c770ec775bba68
Adarsh Maurya <97846000+adarshmaurya18@users.noreply.github.com>
2025-06-08 11:33:45 +0530

    Update and rename Bubble Sort to Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -6,11 +6,15 @@ int main() {
     int arr[n] = {13,46,24,52,20,9};
     for(int i =0;i<n-1;i++)
     {
+        int noswap = 0;
         for(int j = 0;j<n-1-i;j++)
           {
               if(arr[j]>arr[j+1])
                swap(arr[j],arr[j+1]);
+               noswap = 1;
           }
+      if(noswap ==0)
+         break;
     }
     for(int i =0;i<n;i++)
     {
